b43ae496233db6735fe4ce4679f6c575b5c7fe9b
TarunByte <157296076+TarunByte@users.noreply.github.com>
2024-02-27 21:58:04 +0530

    Add files via upload

---
+++ conversionOperation.js
@@ -0,0 +1,50 @@
+let score = true
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+
+// "33" => 33
+// "33abc" => NaN 
+// true = 1; false = 0;
+
+let isLoggedIn = "hitesh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+//"" => false "Hitesh" = true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+
+ //**************************************************** OPERATIONS ***************************************************** */
+
+ let value = 3
+
+ let negValue = -value
+
+ console.log(negValue)
+//2 to the power 2 
+ console.log(2**2)
+
+ let str1 = "Hello"
+ let str2 = " Hitesh"
+
+ let str3 = str1 + str2
+//  console.log(str3)
+
+//  console.log("1" + 2);
+//  console.log(1 + "2");
+//  console.log("1" + 2 + 2);
+//  console.log(1 + 2 + "2");
+
---
+++ dataTypes.js
@@ -0,0 +1,12 @@
+"use strict"; // treat all js codes as newer version
+
+// alert(3+3) we are using nodejs, not browser 
+
+console.log(3 + 3)
+
+console.log("Devloper");
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state = null
